d3aec280464dc8468ffb39d8333761f1fd871196
kwarsanaut <109350649+kwarsanaut@users.noreply.github.com>
2025-08-10 00:46:01 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+## lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/home_screen.dart';
+import 'services/api_service.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<ApiService>(create: (_) => ApiService()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Template',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          useMaterial3: true,
+        ),
+        home: HomeScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
